45cd9b8cd158bac67f9dbe964609233adc0c2e97
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-09 21:20:34 +0300

    Adjust the cors settings

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 // npm imports
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 // file imports
 const router = require("./src/routers");
@@ -13,6 +15,13 @@ require("./src/configs/dbConnection");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    // origin: process.env.CLIENT_URL,
+    // credentials: true,
+  })
+);
 
 // router
 app.use("/api", router);
