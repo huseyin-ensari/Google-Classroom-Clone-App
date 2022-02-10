e4fb4e61dffc9f9df8db3df7b44b6373b42820fb
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-10 14:58:39 +0300

    Add classroom model

---
+++ backend/src/models/Classroom.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { nanoid } = require("nanoid");
+
+const ClassroomSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: String,
+  teacher: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  students: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  accessCode: String,
+});
+
+ClassroomSchema.pre("save", function (next) {
+  if (this.accessCode) return next();
+  this.accessCode = nanoid(5).toUpperCase();
+  next();
+});
+
+const Classroom = mongoose.model("Classroom", ClassroomSchema);
+
+module.exports = Classroom;
