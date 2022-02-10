98b5fcb584696cf6ac2097e09fab92f18b61ee61
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-10 12:49:06 +0300

    Add logout process

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 const UserSchema = new Schema({
   name: {
