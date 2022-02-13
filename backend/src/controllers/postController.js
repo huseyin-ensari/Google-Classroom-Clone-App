4b98cd3bb568f983ce8e5b33b7c011cec20cce11
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-13 16:46:21 +0300

    Use checkExist middleware

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -1,13 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const CustomError = require("../helpers/errors/CustomError");
 const Post = require("../models/Post");
-const Classroom = require("../models/Classroom");
 
 const createPost = asyncHandler(async (req, res, next) => {
   const { classroomID } = req.params;
   const { title, content } = req.body;
-  const classroom = await Classroom.findById(classroomID);
-  if (!classroom) return next(new CustomError("Classroom not found", 400));
+  const classroom = req.classroom;
   const newPost = await Post({ title, content });
   newPost.author = req.user.id;
   req.file ? (newPost.file = req.file.filename) : null;
@@ -18,25 +16,20 @@ const createPost = asyncHandler(async (req, res, next) => {
 });
 
 const deletePost = asyncHandler(async (req, res, next) => {
-  const { classroomID, postID } = req.params;
-  const classroom = await Classroom.findById(classroomID);
-  const post = await Post.findById(postID);
-  if (!classroom || !post) {
-    return next(new CustomError("Classroom or post not found", 400));
-  }
+  const classroom = req.classroom;
+  const post = req.post;
   if (req.user.id !== post.author.toString()) {
     return next(new CustomError("You are not authorized", 400));
   }
-  classroom.posts.splice(classroom.posts.indexOf(postID), 1);
+  classroom.posts.splice(classroom.posts.indexOf(post._id), 1);
   await classroom.save();
   await post.remove();
   return res.status(200).json({ success: true });
 });
 
 const updatePost = asyncHandler(async (req, res, next) => {
-  const { postID } = req.params;
   const { title, content } = req.body;
-  const post = await Post.findById(postID);
+  const post = req.post;
   title ? (post.title = title) : null;
   content ? (post.content = content) : null;
   req.file ? (post.file = req.file.filename) : null;
--- backend/src/routers/postRouter.js
+++ backend/src/routers/postRouter.js
@@ -3,6 +3,10 @@ const inputValidator = require("../middlewares/inputValidations/inputValidations
 const { isAuth } = require("../middlewares/auth/auth");
 const postContoller = require("../controllers/postController");
 const uploadFile = require("../middlewares/assets/uploadFile");
+const {
+  classroomCheck,
+  postCheck,
+} = require("../middlewares/database/checkExist");
 
 // /api/posts
 
@@ -10,15 +14,23 @@ router.post(
   "/:classroomID",
   isAuth,
   uploadFile.single("post_file"),
+  classroomCheck,
   postContoller.createPost
 );
 
-router.delete("/:classroomID/:postID", isAuth, postContoller.deletePost);
+router.delete(
+  "/:classroomID/:postID",
+  isAuth,
+  classroomCheck,
+  postCheck,
+  postContoller.deletePost
+);
 
 router.patch(
   "/:postID",
   isAuth,
   uploadFile.single("post_file"),
+  postCheck,
   postContoller.updatePost
 );
 
