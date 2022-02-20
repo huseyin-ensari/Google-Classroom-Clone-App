62255d09501abb15352e68e02ea3abe040938f74
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-20 04:18:36 +0300

    Add footer section for landing page

---
+++ frontend/src/pages/LandingPage/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer className="bg-light text-center fw-bold p-5">
+      <Container>Copyrigth &copy; Hüseyin Ensari</Container>
+    </footer>
+  );
+};
+
+export default Footer;
