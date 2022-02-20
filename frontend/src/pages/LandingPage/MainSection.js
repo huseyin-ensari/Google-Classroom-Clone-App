d5bcf7fca5a4096b911310c0da6c56508cbb00e9
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-20 04:17:57 +0300

    Add main section for landing page

---
+++ frontend/src/pages/LandingPage/MainSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { mainSvg } from "./assets";
+
+const MainSection = () => {
+  return (
+    <section className="bg-light p-5">
+      <Container>
+        <Row className="justify-content-center align-items-center">
+          <Col className="text-center me-5">
+            <h2 className="mb-4">
+              The platform where everything related to education and learning
+              comes together
+            </h2>
+            <p className="lead">
+              <span className="text-primary fw-bold">Classroom</span> is a
+              one-stop shop for all things education and learning. With our
+              easy-to-use and secure tool, instructors manage, measure and
+              enrich their learning experience.
+            </p>
+            <Button>Join Us</Button>
+          </Col>
+          <Col>
+            <Image fluid src={mainSvg} alt="Online Education | Classroom App" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default MainSection;
