dead0b313ffe03dbf8d6ef0dac323eae213d76e3
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-20 04:18:54 +0300

    Create landing page

---
+++ frontend/src/pages/LandingPage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CardsSection from "./CardsSection";
+import Footer from "./Footer";
+import MainSection from "./MainSection";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <MainSection />
+      <CardsSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
