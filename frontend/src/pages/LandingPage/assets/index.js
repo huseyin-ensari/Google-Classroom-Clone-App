8a3dd6c5ae90cf39a7afbda5e68f894fb52e7a7a
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-20 04:17:33 +0300

    Add assets for landing page

---
+++ frontend/src/pages/LandingPage/assets/index.js
@@ -0,0 +1,5 @@
+export { default as mainSvg } from "./main.svg";
+export { default as downloadExcel } from "./download_excel.svg";
+export { default as giveNote } from "./give_a_note.svg";
+export { default as lectureNotes } from "./lecture_notes.svg";
+export { default as shareYourHomework } from "./share_your_homework.svg";
