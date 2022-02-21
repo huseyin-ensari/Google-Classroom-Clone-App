1fdaefe3919554f187475e8c9170e6ac7cb47d47
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-22 02:57:24 +0300

    Add fetchLogin function and fix some spellings

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-export const register = async (inputs) => {
-  const result = axios.post(
-    process.env.REACT_APP_BASE_URL + "/api/users/register",
-    inputs
-  );
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
+export const fetchRegister = async (inputs) => {
+  const result = await axios.post(`${BASE_URL}/api/users/register`, inputs);
+  return result;
+};
+
+export const fetchLogin = async (inputs) => {
+  const result = await axios.post(`${BASE_URL}/api/users/login`, inputs);
   return result;
 };
