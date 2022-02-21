ae67242d7870e10571c09ad50643754b1e8f3f67
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-22 02:58:23 +0300

    Create home page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as LandingPage } from "./LandingPage";
 export { default as LoginPage } from "./LoginPage";
 export { default as RegisterPage } from "./RegisterPage";
+export { default as HomePage } from "./HomePage";
