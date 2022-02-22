ba03778d0230f08f367f7d4326829b57d65b9c81
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-22 03:01:20 +0300

    Let the homepage of logged in users be <HomePage>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import { Navigation } from "./components";
 import { Routes, Route } from "react-router-dom";
-import { LandingPage, LoginPage, RegisterPage } from "./pages/index";
+import { LandingPage, LoginPage, RegisterPage, HomePage } from "./pages/index";
+import { useContext } from "react";
+import { AuthContext } from "./contexts/authContext";
 
 function App() {
+  const { isLoggin } = useContext(AuthContext);
+
   return (
     <div className="App">
       <Navigation />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={isLoggin ? <HomePage /> : <LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
