801d60af6c8fb95845978b89c65250bf1d6d5563
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-25 23:40:49 +0300

    Add logout button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,16 +1,32 @@
 import React, { useContext } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { BsPersonCircle } from "react-icons/bs";
+import { RiLogoutCircleRLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/authContext";
 
 const ProfileButton = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
+  };
 
   return (
-    <Button variant="outline-secondary" className="ms-auto">
-      <BsPersonCircle className="me-3" />
-      {user.name} {user.lastname}
-    </Button>
+    <Stack className="ms-auto" direction="horizontal" gap={2}>
+      <Button
+        variant="outline-secondary"
+        className="ms-auto btn-outline-primary"
+      >
+        <BsPersonCircle className="me-3" />
+        {user.name} {user.lastname}
+      </Button>
+      <Button variant="secondary" onClick={handleLogout}>
+        <RiLogoutCircleRLine />
+      </Button>
+    </Stack>
   );
 };
 
