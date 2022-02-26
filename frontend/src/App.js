eb1a379eefa972a0ac238e94fb61c620459634ec
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-27 01:53:48 +0300

    Use route layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { LandingPage, LoginPage, RegisterPage, HomePage } from "./pages/index";
 import { useContext } from "react";
 import { AuthContext } from "./contexts/authContext";
+import { PrivateRoutes, NonPrivateRoutes } from "./configs/routeLayout";
 
 function App() {
   const { isLoggin } = useContext(AuthContext);
@@ -12,8 +13,15 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={isLoggin ? <HomePage /> : <LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        {/* NonPrivateRoutes */}
+        <Route element={<NonPrivateRoutes isLoggin={isLoggin} />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+        {/* End of NonPrivateRoutes */}
+        {/* PrivateRoutes */}
+        <Route element={<PrivateRoutes isLoggin={isLoggin} />}></Route>
+        {/* End of PrivateRoutes */}
       </Routes>
     </div>
   );
