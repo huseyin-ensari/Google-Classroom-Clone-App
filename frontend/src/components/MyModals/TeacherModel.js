4e27f05500de70896c14bf744c4c31509935fc58
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-26 03:42:49 +0300

    Creating modal component for teachers

---
+++ frontend/src/components/MyModals/TeacherModel.js
@@ -0,0 +1,86 @@
+import { useFormik } from "formik";
+import React from "react";
+import { Form, Modal, FloatingLabel, Button } from "react-bootstrap";
+import { fetchCreateClassroom } from "../../api/classroomApi";
+import { classroomValidation } from "../../helpers/inputValidation.js";
+
+const TeacherModel = (props) => {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      subtitle: "",
+    },
+    validationSchema: classroomValidation,
+    onSubmit: async (values, bag) => {
+      try {
+        const response = await fetchCreateClassroom(values);
+        console.log("sınıf oluşturma -> ", response);
+      } catch (e) {}
+      values.title = "";
+      values.subtitle = "";
+      props.onHide();
+    },
+  });
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Create Classroom
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {/* form */}
+        <Form>
+          <FloatingLabel
+            controlId="floatingTitle"
+            label="Classroom Title"
+            className="mb-3"
+          >
+            <Form.Control
+              onChange={formik.handleChange}
+              onBlur={formik.onBlur}
+              value={formik.values.title}
+              name="title"
+              type="text"
+              isInvalid={formik.touched.title && formik.errors.title}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.title}
+            </Form.Control.Feedback>
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingSubtitle"
+            label="Subtitle"
+            className="mb-3"
+          >
+            <Form.Control
+              onChange={formik.handleChange}
+              onBlur={formik.onBlur}
+              value={formik.values.subtitle}
+              name="subtitle"
+              type="text"
+              isInvalid={formik.touched.subtitle && formik.errors.subtitle}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.subtitle}
+            </Form.Control.Feedback>
+          </FloatingLabel>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-primary" onClick={formik.handleSubmit}>
+          Create
+        </Button>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default TeacherModel;
---
+++ frontend/src/components/MyModals/index.js
@@ -0,0 +1,1 @@
+export { default as TeacherModel } from "./TeacherModel";
