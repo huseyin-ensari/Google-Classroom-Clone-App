a928efaad4e0129e701d0b320ae5d1b31861e3af
Hüseyin Ensari <hsynensari@gmail.com>
2022-02-28 19:47:34 +0300

    Create offcanvas component for classroom update

---
+++ frontend/src/components/MyOffCanvas/EditClassroom.js
@@ -0,0 +1,73 @@
+import { useContext, useEffect, useState } from "react";
+import { Offcanvas, Button, Form } from "react-bootstrap";
+import { useFormik } from "formik";
+import { AiFillEdit } from "react-icons/ai";
+import { updateClassroomValidation } from "../../helpers/inputValidation.js";
+import { fetchUpdateClassroomInformation } from "../../api/classroomApi.js";
+import { AuthContext } from "../../contexts/authContext";
+
+const EditClassroom = ({ classroom }) => {
+  const [show, setShow] = useState(false);
+  const { setClassroom } = useContext(AuthContext);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: updateClassroomValidation,
+    onSubmit: async (values) => {
+      setShow(false);
+      const { data } = await fetchUpdateClassroomInformation(
+        classroom._id,
+        values
+      );
+      setClassroom({ ...classroom, title: data.classroom.title });
+    },
+  });
+
+  return (
+    <>
+      <Button size="sm" onClick={handleShow}>
+        <AiFillEdit />
+      </Button>
+
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        className="w-75 h-75 mx-auto p-5"
+        placement="top"
+      >
+        <Offcanvas.Header closeButton={true}>
+          <Offcanvas.Title>Edit Classroom Title</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Form onSubmit={formik.handleSubmit}>
+            <Form.Group>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                onBlur={formik.onBlur}
+                name="title"
+                onChange={formik.handleChange}
+                value={formik.values.title}
+                isInvalid={formik.touched.title && formik.errors.title}
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.title}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <div className="d-grid gap-2">
+              <Button type="submit" className="mt-2">
+                Update
+              </Button>
+            </div>
+          </Form>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+};
+
+export default EditClassroom;
---
+++ frontend/src/components/MyOffCanvas/index.js
@@ -0,0 +1,1 @@
+export { default as EditClassroomInformation } from "./EditClassroom";
