f7f78ce6da67777bbc864c6827e760e60548d927
Hüseyin Ensari <hsynensari@gmail.com>
2022-03-03 23:28:38 +0300

    Create homework creation function from api

---
+++ frontend/src/api/homeworkApi.js
@@ -0,0 +1,12 @@
+import { api } from "./authApi";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const fetchCreateHomework = async (classroomID, inputs) => {
+  const result = await api.post(
+    `${BASE_URL}/api/homeworks/${classroomID}`,
+    inputs
+  );
+
+  return result;
+};
