6ea59c07540162ce0111a88eb2950362c9746a48
Hüseyin Ensari <hsynensari@gmail.com>
2022-03-04 00:47:23 +0300

    Add the section where the homeworks will be listed

---
+++ frontend/src/pages/ClassroomPage/CreateHomework.js
@@ -0,0 +1,70 @@
+import React, { useContext } from "react";
+import { Button, Form, Accordion } from "react-bootstrap";
+import { useFormik } from "formik";
+import { fetchCreateHomework } from "../../api/homeworkApi";
+import { AuthContext } from "../../contexts/authContext";
+
+const CreateHomework = () => {
+  const { classroom } = useContext(AuthContext);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      content: "",
+      endTime: "",
+    },
+    onSubmit: async (values, bag) => {
+      const response = await fetchCreateHomework(classroom._id, values);
+      console.log("response ", response);
+    },
+  });
+
+  return (
+    <Accordion defaultActiveKey={0} className="mt-3">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Create Post</Accordion.Header>
+        <Accordion.Body>
+          {/* Form */}
+          <Form
+            className="px-5"
+            onSubmit={formik.handleSubmit}
+            encType="multipart/form-data"
+          >
+            {/* Title */}
+            <Form.Group className="mt-2">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                onChange={formik.handleChange}
+                type="text"
+                name="title"
+              />
+            </Form.Group>
+            <Form.Group className="mt-2">
+              <Form.Label>Content</Form.Label>
+              <Form.Control
+                onChange={formik.handleChange}
+                type="text"
+                name="content"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 mt-2">
+              <Form.Label>End Time</Form.Label>
+              <Form.Control
+                onChange={formik.handleChange}
+                type="date"
+                name="endTime"
+              />
+            </Form.Group>
+            <div className="d-grid gap-2">
+              <Button size="sm" type="submit">
+                Send
+              </Button>
+            </div>
+          </Form>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+};
+
+export default CreateHomework;
--- frontend/src/pages/ClassroomPage/HomeworkTab.js
+++ frontend/src/pages/ClassroomPage/HomeworkTab.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/authContext";
+import CreateHomework from "./CreateHomework";
+import { Card, Row, Col } from "react-bootstrap";
+import moment from "moment";
+import SubmitHomeworkOffCanvas from "../../components/MyOffCanvas/SubmitHomeworkOffCanvas";
 
 const HomeworkTab = ({ homeworks }) => {
-  return <div>HomeworkTab</div>;
+  const { user } = useContext(AuthContext);
+
+  return (
+    <>
+      {user.role === "teacher" && <CreateHomework />}
+      {homeworks.length === 0 ? (
+        <p className="fw-bold mt-2 text-center">There are no homework.</p>
+      ) : (
+        <>
+          {homeworks.reverse().map((homework) => (
+            <Card className="mt-2" key={homework._id}>
+              <Card.Body>
+                <Card.Title>
+                  <h5 className="d-inline me-3">{homework.title}</h5>
+                </Card.Title>
+                <Card.Text>{homework.content}</Card.Text>
+              </Card.Body>
+              <Card.Footer className="text-muted">
+                <Row>
+                  <Col>
+                    <SubmitHomeworkOffCanvas homeworkID={homework._id} />
+                  </Col>
+                  <Col className="text-end">
+                    THE LAST DAY {moment(homework.endTime).format("DD.MM.YYYY")}
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
+          ))}
+        </>
+      )}
+    </>
+  );
 };
 
 export default HomeworkTab;
