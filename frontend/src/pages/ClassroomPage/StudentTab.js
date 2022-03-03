7476a284c4392fcf6d90cc3a9f0e8bb938d851b6
Hüseyin Ensari <hsynensari@gmail.com>
2022-03-04 00:46:25 +0300

    Add the section where students will be listed

--- frontend/src/pages/ClassroomPage/StudentTab.js
+++ frontend/src/pages/ClassroomPage/StudentTab.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 
 const StudentTab = ({ students }) => {
-  return <div>StudentTab</div>;
+  return (
+    <>
+      {students.length === 0 ? (
+        <p className="fw-bold mt-2 text-center">There are no posts.</p>
+      ) : (
+        <>
+          <Table striped bordered hover size="sm" className="mt-3">
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.map((student) => (
+                <tr key={student._id}>
+                  <td>{student.name}</td>
+                  <td>{student.lastname}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <p className="fw-bold mt-2 text-center">
+            There are {students.length} student(s)
+          </p>
+        </>
+      )}
+    </>
+  );
 };
 
 export default StudentTab;
